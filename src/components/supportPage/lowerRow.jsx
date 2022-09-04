52e4835c185e310fc9b89d9f5c6756fb5ac6947d
kratos1710 <sbhandari1907@protonmail.com>
2022-09-05 00:32:19 +0530

    low

--- src/components/supportPage/lowerRow.jsx
+++ src/components/supportPage/lowerRow.jsx
@@ -2,9 +2,11 @@ export default function (props) {
   return (
     <div className="relative text-white">
       <img src={props.img} alt="" />
-      <div className="w-full h-full flex items-end justify-start absolute top-0 left-0">
+      <div className="w-full h-full flex items-end justify-start md:items-center absolute top-0 left-0">
         <div className="p-4 pb-6">
-          <div className="text-2xl font-semibold">{props.heading}</div>
+          <div className="md:text-base text-2xl font-semibold">
+            {props.heading}
+          </div>
           {props.text ? <div className="text-sm">{props.text}</div> : null}
         </div>
       </div>
