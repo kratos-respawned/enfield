b7b6953b484eeb848634273f0dd3b0749886d4f0
kratos1710 <sbhandari1907@protonmail.com>
2022-09-04 23:24:43 +0530

    added links

--- src/Pages/Support.jsx
+++ src/Pages/Support.jsx
@@ -1,54 +1,52 @@
 import ImageBox from "../components/supportPage/ImageBox";
 export default function Support() {
+  const upperRow = [
+    {
+      img: "https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/tout/support-call.jpg",
+      heading: "Contact Us",
+      centerImg:
+        "	https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/logos/call.svg",
+      innerText:
+        "For assistance on all things Enfield, contact us at: 1800 210 0007 (Roadside Assistance 24X7 & Customer Care 9 AM - 9 PM)",
+    },
+    {
+      img: "https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/tout/support-write-to-us.jpg",
+      heading: "Write To us",
+      centerImg:
+        "https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/logos/mail.svg",
+      innerText:
+        "Email your query, feedback or suggestion to support@royalenfield.com<",
+    },
+    {
+      img: "https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/tout/support-faq.jpg",
+      heading: "FAQs",
+      centerImg:
+        "https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/logos/faq.svg",
+      innerText: "Get answers to the most commonly asked questions",
+    },
+    {
+      img: "https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/tout/support-rsa.JPG",
+      heading: "Road Side Assistance",
+      centerImg:
+        "https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/logos/rsa.svg",
+      innerText:
+        "Find details about the emergency roadside assistance programme",
+    },
+  ];
   return (
     <>
       <div className="flex flex-col md:flex-row md:flex-wrap text-black">
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-      </div>
-      <div className="flex flex-col md:flex-row md:flex-wrap text-black">
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-      </div>
-      <div className="flex flex-col md:flex-row md:flex-wrap text-black">
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-      </div>
-      <div className="flex flex-col md:flex-row md:flex-wrap text-black">
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-      </div>
-      <div className="flex flex-col md:flex-row md:flex-wrap text-black">
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-      </div>
-      <div className="flex flex-col md:flex-row md:flex-wrap text-black">
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-      </div>
-      <div className="flex flex-col md:flex-row md:flex-wrap text-black">
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-      </div>
-      <div className="flex flex-col md:flex-row md:flex-wrap text-black">
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
-        <ImageBox />
+        {upperRow.map((card, key) => {
+          return (
+            <ImageBox
+              key={key}
+              img={card.img}
+              text={card.innerText}
+              heading={card.heading}
+              centerImg={card.centerImg}
+            />
+          );
+        })}
       </div>
     </>
   );
--- src/components/supportPage/ImageBox.jsx
+++ src/components/supportPage/ImageBox.jsx
@@ -1,21 +1,21 @@
 import "../style.css";
-export default function ImageBox({ img }) {
+export default function ImageBox(props) {
   return (
     <>
       <div className="relative h-[380px] w-[100%] md:h-[250px] md:w-[25%] xl:hidden bg-red-500 flex flex-col">
         <div className="relative h-full w-full grid place-items-center overflow-hidden">
           <img
             className="absolute top-0 left-0 h-full w-full hover:scale-110 transition-transform transform-gpu duration-[3000ms] cursor-pointer "
-            src="https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/tout/support-call.jpg"
+            src={props.img}
             alt=""
           />
           <img
-            src="https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/logos/call.svg"
+            src={props.centerImg}
             className="w-[30vw] md:w-[50px] lg:w-[65px] xl:w-[96px] z-20  -translate-y-3"
           />
           <div className="xl:hidden h-[100px] w-full absolute bottom-0 bg-[rgba(33,33,33)] flex flex-col items-center py-[10px]">
             <div className="text-white font-Guardian-Medium text-lg">
-              Classic 350
+              {props.heading}
             </div>
             <div className="border border-[white] h-[35px] w-[100px]  hover:bg-white hover:text-[#d1291c] grid place-items-center text-white font-Montserrat text-xs font-[600] tracking-wide transition-all duration-1000 cursor-pointer mt-3">
               Know More &gt;
@@ -27,20 +27,19 @@ export default function ImageBox({ img }) {
         <div className="relative h-full w-full grid place-items-center overflow-hidden transition-all duration-500">
           <img
             className="absolute top-0 left-0 h-full w-full hover:scale-110 transition-transform transform-gpu duration-[3000ms] cursor-pointer "
-            src="https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/tout/support-call.jpg"
+            src={props.img}
             alt=""
           />
           <img
-            src="https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/logos/call.svg"
+            src={props.centerImg}
             className="w-[30px] md:w-[50px] lg:w-[65px] xl:w-[96px] z-20  -translate-y-3"
           />
           <div className="bottomBox h-[200px] z-30 w-full absolute -bottom-20 bg-[rgba(33,33,33)] flex flex-col items-center py-[10px] transition-all duration-500">
             <div className="text-white font-Guardian-Medium text-lg">
-              Classic 350
+              {props.heading}
             </div>
             <div className="mt-2 w-[85%] text-white font-Montserrat min-h-[65px] text-[13px] leading-[22px] font-semibold break-words text-center px-[30px]">
-              For assistance on all things Enfield, contact us at: 1800 210 0007
-              (Roadside Assistance 24X7 &amp; Customer Care 9 AM - 9 PM)
+              {props.text}
             </div>
             <div className="border border-[white] h-[35px] w-[100px] hover:bg-white hover:text-[#d1291c] grid place-items-center text-white font-Montserrat text-xs font-[600] tracking-wide transition-all duration-1000 cursor-pointer mt-5">
               Explore &gt;
