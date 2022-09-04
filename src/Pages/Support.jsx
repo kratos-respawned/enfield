a73f57e82c7ec1ced6860137b2c970a22bda9bb7
kratos1710 <sbhandari1907@protonmail.com>
2022-09-04 12:03:39 +0530

    support card improvements

--- src/Pages/Support.jsx
+++ src/Pages/Support.jsx
@@ -2,11 +2,11 @@ import ImageBox from "../components/supportPage/ImageBox";
 export default function Support() {
   return (
     <>
-      <div className="flex flex-col  text-black">
+      <div className="flex flex-col md:flex-row md:flex-wrap text-black">
         <ImageBox />
-        {/* <ImageBox  />
-        <ImageBox  />
-        <ImageBox  /> */}
+        <ImageBox />
+        <ImageBox />
+        <ImageBox />
       </div>
     </>
   );
--- src/components/supportPage/ImageBox.jsx
+++ src/components/supportPage/ImageBox.jsx
@@ -1,56 +1,33 @@
 export default function ImageBox({ img }) {
   return (
     <>
-      <div className="flex-1 w-full text-white ">
-        <div className="relative w-full h-full overflow-hidden">
+      <div className="flex-1  relative w-full text-white  ">
+        <div className="relative flex-1 overflow-hidden">
           <img
-            className="w-full cursor-pointer hover:scale-105 transition-transform  transform-gpu duration-[3000ms]"
+            className="w-full cursor-pointer scale-105 hover:scale-110 transition-transform  transform-gpu duration-[3000ms]"
             src="https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/tout/support-call.jpg"
           />
 
-          <div className="pointer-events-none absolute grid place-items-center w-full h-full z-20 bg-[#3636365c] top-0  left-0">
+          <div className="pointer-events-none absolute grid place-items-center md:items-end md:pb-4 w-full h-full z-20 bg-[#2a292988] top-0  left-0">
             <img
               src="https://www.royalenfield.com/content/dam/royal-enfield/india/support/home/logos/call.svg"
-              width="200px"
+              className="w-[30vw] md:w-[50px] lg:w-[65px] xl:w-[96px] "
             />
           </div>
         </div>
 
-        <div className="h-[90px] w-full  bg-black flex flex-col items-center py-[10px]">
-          <div className=" font-Guardian-Medium text-lg">Classic 350</div>
-          <div className="border   border-[white] h-[35px] px-5  hover:text-red-500 flex items-center text-white font-Montserrat text-xs font-[600] tracking-wide transition-colors duration-500 cursor-pointer mt-2">
-            <div>Explore</div>
+        <div className="  h-full w-full  px-[15px] z-30 bg-[#212121] flex flex-col items-center py-[10px]">
+          <div className=" font-Guardian-Medium text-lg">Contact Us</div>
+          <div className="hidden xl:block min-h-[65px] text-[13px] leading-[22px] font-semibold break-words text-center px-[30px]">
+            For assistance on all things Enfield, contact us at: 1800 210 0007
+            (Roadside Assistance 24X7 &amp; Customer Care 9 AM - 9 PM)
+          </div>
+          <div className="border  border-[white] h-[35px] px-5  hover:text-red-500 flex items-center text-white font-Montserrat text-xs font-[600] tracking-wide transition-colors duration-500 cursor-pointer mt-2">
+            <div>Know More</div>
             <div className="ml-[5px] pt-[1px]">&gt;</div>
           </div>
         </div>
       </div>
     </>
   );
-}
-
-//
-{
-  /* <div className="flex-1 text-white relative basis-1/3 w-full overflow-hidden">
-      <div
-        className={` absolute z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 `}
-      >
-        <div className="w-fit h-fit">Center Img</div>
-      </div>
-      <div className="overflow-hidden backdrop-brightness-50 z-50">
-        <img
-          className="min-w-full hover:scale-110 duration-[5000ms] transition-transform transform-gpu"
-          src={img}
-          alt=""
-          srcset=""
-        />
-      </div>
-      <div className="bg-[#212121] z-50 w-full py-2">
-        <div className="text-center text-lg z-50 font-semibold mb-2">
-          Contact Us
-        </div>
-        <div className="mx-auto w-fit px-7 py-1  z-50 border-[1px]">
-          Know More
-        </div>
-      </div>
-    </div> */
 }
