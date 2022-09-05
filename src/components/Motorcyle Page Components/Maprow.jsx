c22f2b2d976394b3e803dfb9b677e9497c2f26c8
kratos1710 <sbhandari1907@protonmail.com>
2022-09-05 09:21:53 +0530

    minor changes

--- src/components/Motorcyle Page Components/Maprow.jsx
+++ src/components/Motorcyle Page Components/Maprow.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
+import React from "react";
 
 import MapItems from "./MapItems";
 export default function Maprow() {
-    return (
-        <>
-
-            <div className="flex flex-row flex-wrap items-center justify-evenly w-full h-full py-10 md:py-20 bg-[#212121]">
-
-                <MapItems />
-                <MapItems />
-                <MapItems />
-                <MapItems />
-            
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex flex-row flex-wrap items-center justify-evenly w-full h-full py-10 md:py-20 bg-[#212121]">
+        <MapItems text="Book a Test Ride" />
+        <MapItems text="Find a Store" />
+        <MapItems text="Ride Out" />
+        <MapItems text="Book a Service" />
+      </div>
+    </>
+  );
 }
